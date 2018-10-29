485faf590fb8e4f01af9d7d0c9cd3e1193fd7307
Paul <hhiinaarruu@gmail.com>
2018-10-29 12:25:59 +0700

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+class UsersSignupTest < ActionDispatch::IntegrationTest
 
- test "invalid signup information" do
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
@@ -11,8 +12,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-     follow_redirect!
+    assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+  end
+ test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
     assert_template 'users/show'
-     assert_not flash.FILL_IN
+    assert_not flash.FILL_IN
   end
 end
